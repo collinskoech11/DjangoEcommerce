736a7f1328de03c983cdd1937d701d2da0e805b9
collinskoech11 <collinskoechck34@gmail.com>
2021-07-11 17:44:19 +0300

    url typo fix

--- ecomsite/ecomsite/urls.py
+++ ecomsite/ecomsite/urls.py
@@ -20,6 +20,6 @@ from shop import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
-    path('<int:id>/',views.detail,name='detai'),
+    path('<int:id>/',views.detail,name='detail'),
     path('checkout/',views.checkout,name='checkout')
 ]

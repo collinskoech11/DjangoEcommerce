3d93de3f1268da87aa65c654b9565679db433d22
collinskoech11 <collinskoechck34@gmail.com>
2021-07-11 07:55:32 +0300

    added checkout.html

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -20,3 +20,7 @@ def index(request):
 def detail(request, id):
     product_object = Products.objects.get(id=id)
     return render(request, 'shop/detail.html', {'product_object':product_object})
+
+
+def checkout(request):
+    return render(request,'shop/checkout.html')    
